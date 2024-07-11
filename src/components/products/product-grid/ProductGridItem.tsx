7199453746b373de888d6b7e791add1fa12409fb
alan damian Fernandez <92124271+Alan-Fernandez@users.noreply.github.com>
2024-07-11 09:29:16 -0300

    correccion

--- src/components/products/product-grid/ProductGridItem.tsx
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -16,7 +16,7 @@ export const ProductGridItem = ( { product }: Props ) => {
 
   const [ displayImage, setDisplayImage ] = useState( product.images[ 0 ] );
 
-
+  console.log(`displayImage: `,displayImage)
   return (
     <div className="rounded-md overflow-hidden fade-in">
       <Link href={ `/product/${ product.slug }` }>
